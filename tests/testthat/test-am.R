546cb88adc7bd9dcb639bc818be7d93baa4b4e11
jangorecki <j.gorecki@wit.edu.pl>
2015-05-09 02:57:35 +0100

    another test detects

--- tests/testthat/test-am.R
+++ tests/testthat/test-am.R
@@ -5,7 +5,7 @@ test_that("AM basic construct", {
     am <- AM$new()
     expect_true(nrow(am$log)==1L, info="AM logging1")
     am$create(class = "anchor", mne = "AC", desc = "Actor")
-    am$create(class = "attribute", mne = "AC_GEN", desc = "Gender")
+    am$create(class = "attribute", anchor = "AC", mne = "GEN", desc = "Gender")
     expect_true(nrow(am$log)==3L, info="AM logging2")
     expect_true(all(sapply(am$data$obj, is.am)), info="AM all obj are MAobj")
 
