bea1c6045e13ad20693d9b8980543a786d94b3bb
jangorecki <j.gorecki@wit.edu.pl>
2015-05-09 01:30:05 +0100

    verbose usage of keys in data.table

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onLoad <- function(libname, pkgname){
 
     options("am.size.format" = NULL) # NULL means auto on individual rows
+    options("am.key.verbose" = FALSE) # interactive monitor for key/key2 usage, will be changed to FALSE on stable
 
 }
