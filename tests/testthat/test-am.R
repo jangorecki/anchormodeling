f147adb3ca05b430ae3d5e5003d61449306e2ded
jangorecki <j.gorecki@wit.edu.pl>
2015-05-24 02:35:12 +0100

    log for IM not updated in tests

--- tests/testthat/test-am.R
+++ tests/testthat/test-am.R
@@ -3,10 +3,10 @@ context("AM anchormodeling")
 test_that("AM basic construct", {
 
     am <- AM$new()
-    expect_true(nrow(am$log)==1L, info="AM logging1")
+    expect_true(nrow(am$log)==2L, info="AM logging1")
     am$create(class = "anchor", mne = "AC", desc = "Actor")
     am$create(class = "attribute", anchor = "AC", mne = "GEN", desc = "Gender")
-    expect_true(nrow(am$log)==3L, info="AM logging2")
+    expect_true(nrow(am$log)==4L, info="AM logging2")
     expect_true(all(sapply(am$data$obj, is.am)), info="AM all obj are MAobj")
 
 })
