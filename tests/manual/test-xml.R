1822440d6639f461e5ab0d8cb9ea3d1d54ff0630
jangorecki <j.gorecki@wit.edu.pl>
2015-05-27 21:57:44 +0100

    remove pkg prefix to already exported fun

--- tests/manual/test-xml.R
+++ tests/manual/test-xml.R
@@ -2,7 +2,7 @@ context("xml")
 
 test_that("xml valid for Anchor Modeler", {
 
-    am <- anchormodeling:::actor.am(Inf)
+    am <- actor.am(Inf)
     file <- am$xml()
     message("File ",file," is waiting in your wd to be loaded in web browser on ",anchor_modeler_url(),"\nDon't forget to click Play!")
     browseURL(anchor_modeler_url())
