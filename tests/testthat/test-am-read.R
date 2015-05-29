66c470e489d055715a6ec39f80f5b77ecd619cd1
jangorecki <j.gorecki@wit.edu.pl>
2015-05-29 22:07:30 +0100

    unit tests description change

--- tests/testthat/test-am-read.R
+++ tests/testthat/test-am-read.R
@@ -7,11 +7,11 @@ test_that("AM read method", {
     am$create(class = "attribute", anchor = "AC", mne = "NAM", desc = "Name")
     # expected fields returned from $read
     expect_true(all(nrow(am$read()) == 2L, length(am$read())), info = "")
-    expect_identical(names(am$read()), c("code", "name", "class", "mne", "desc", "obj", "hist", "knot"), info = "Defined *non-run* columns exists")
+    expect_identical(names(am$read()), c("code", "name", "class", "mne", "desc", "obj", "hist", "knot"), info = "Before am$run all columns exists")
     am$run()
     am$load(mapping = list(AC = list("code", NAM = "name")),
             data = data.table(code = "1", name = "Mike"),
             meta = 1L)
-    expect_identical(names(am$read()), c("code", "name", "class", "mne", "desc", "obj", "hist", "knot", "anchor", "parents", "childs"), info = "Defined *run* columns exists")
+    expect_identical(names(am$read()), c("code", "name", "class", "mne", "desc", "obj", "hist", "knot", "anchor", "parents", "childs"), info = "After *am$run* all columns exists")
 
 })
