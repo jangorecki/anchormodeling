32966dbcb7b3e4184a02abb5f0779e327a1bbb12
jangorecki <j.gorecki@wit.edu.pl>
2015-05-29 23:09:23 +0100

    more IM unit tests

--- tests/testthat/test-im.R
+++ tests/testthat/test-im.R
@@ -5,6 +5,7 @@ test_that("IM process", {
     set.seed(1)
 
     im <- IM$new()
+    expect_error(im$create(mne = "XX"), "argument \"nk\" is missing, with no default", info = "missing nk arg in im$create")
     im$create(mne = "GEN", nk = "gender")
     im$create(mne = "UL", nk = "user_login")
     data <- data.table(user_login = c("asd","qwe","zxc","asd","zxc","asd"), gender = c("F","M","M","F","M","F"), balance = rnorm(6,10))
@@ -26,10 +27,19 @@ test_that("IM process", {
     expect_identical(im$NK[["AC"]],c("actor_code1", "actor_code2"), info="composite natural key")
     expect_identical(names(data.sk),c("actor_code1", "actor_name", "actor_code2", "AC_ID"), info="composite natural key use method output")
     expect_true(data.sk[is.na(AC_ID), .N == 0L], info="no NA surrogate keys from composite natural key")
-    # auto im$create while im$use
-    # info="auto im$create"
-    # info="tried auto im$create with missing nk"
-    # info="tried auto im$create with missing nk[[mne]]"
+
+})
+
+test_that("auto im$create while im$use", {
+
+    im <- IM$new()
+    im$create(mne = "UL", nk = "user_login")
+    data <- im$use(data = data.table(user_login = c("nick1","nick2")),
+                   mne = "UL")
+    expect_identical(names(data), c("user_login","UL_ID"), info = "result of IM$use produce expected columns on auto im$create on im$use")
+    expect_equal(im$ID[["UL"]], data.table(user_login = c("nick1","nick2"), UL_ID = 1:2, key = "user_login"), info = "expected IDs produced on auto im$create on im$use")
+    expect_error(data <- im$use(data = data.table(user_login = c("nick1","nick2"))), "argument \"mne\" is missing, with no default", info = "missing mne in im$use")
+    expect_error(data <- im$use(mne = "XX"), "argument \"data\" is missing, with no default", info = "missing data in im$use")
 
 })
 
